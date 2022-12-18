9875e786bdc5d448d931a0c6627cd850f8ad11e6
Ion Cernei <jonnycernei@gmail.com>
2022-12-18 16:38:04 +0200

    Add chypher pages

--- src/app/app.py
+++ src/app/app.py
@@ -3,6 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import pyotp
 import joblib
+from src.cyphers.classical_cyphers.caesar import Caesar
+from src.cyphers.classical_cyphers.caesar_with_keyword import CaesarWithKeyword
+from src.cyphers.classical_cyphers.polybius_with_keyword import PolybiusWithKeyword
+from src.cyphers.classical_cyphers.vigenere import Vigenere
+from src.cyphers.symetrical_cyphers.stream.lfsr_str import LfsrStr
+from src.cyphers.symetrical_cyphers.block.des import Des
+from src.cyphers.asymmetrical_cyphers.rsa import Rsa
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(16).hex()
@@ -30,12 +37,12 @@ def register():
     form_pass = request.form.get("password")
     if request.method == 'POST':
         if form_user in database:
-            return 'Usernaem already exists, go to login'
+            return 'Usernaem already exists, go to login page'
         else:
             user = User(form_user, form_pass)
             database[form_user] = user
             return f'Use this code: {user.otp_seed} in your authenticator app'
-    return 'this is registration page'
+    return 'this is the registration page'
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -50,9 +57,9 @@ def login():
         if valid and form_user in database and check_password_hash(database[form_user].password_hash, form_pass):
             session['username'] = form_user
             return 'you can go to index page now'
-        return ("Invalid username, password or code. Please try again.")
+        return "Invalid username, password or code. Please try again."
 
-    return 'this is log in page'
+    return 'this is the login page'
 
 
 @app.route('/logout')
@@ -61,6 +68,107 @@ def logout():
         return 'visit login page first'
     session.pop('username')
     return 'you have succesfuly logout'
+
+
+@app.route("/caesar", methods=['GET', 'POST'])
+def caesar():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        key = int(request.form.get("key"))
+        action = request.form.get("action")
+        cypher = Caesar(key)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using Caesar cypher'
+
+
+@app.route("/caesar_with_keyword", methods=['GET', 'POST'])
+def caesar_with_keyword():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        key = int(request.form.get("key"))
+        keyword = request.form.get("keyword")
+        action = request.form.get("action")
+        cypher = CaesarWithKeyword(key, keyword)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using Caesar cypher with keyword'
+
+
+@app.route("/polybius", methods=['GET', 'POST'])
+def polybius():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        keyword = request.form.get("keyword")
+        action = request.form.get("action")
+        cypher = PolybiusWithKeyword(keyword)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using Polybius cypher with keyword'
+
+
+@app.route("/vigenere", methods=['GET', 'POST'])
+def vigenere():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        keyword = request.form.get("keyword")
+        action = request.form.get("action")
+        cypher = Vigenere(keyword)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using Vigenere cypher'
+
+
+@app.route("/lfsr", methods=['GET', 'POST'])
+def lfsr():
+    if request.method == 'POST':
+        register = request.form.get("register")
+        taps = request.form.get("taps")
+        text = request.form.get("text")
+        action = request.form.get("action")
+        cypher = LfsrStr(register, taps)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using LFSR cypher'
+
+
+@app.route("/des", methods=['GET', 'POST'])
+def des():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        key = request.form.get("key")
+        action = request.form.get("action")
+        cypher = Des(key)
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using Des cypher'
+
+
+@app.route("/rsa", methods=['GET', 'POST'])
+def rsa():
+    if request.method == 'POST':
+        text = request.form.get("text")
+        p = request.form.get("p")
+        q = request.form.get("q")
+        action = request.form.get("action")
+        cypher = Rsa(int(p), int(q))
+        if action == 'encrypt':
+            return cypher.encrypt(text)
+        if action == 'decrypt':
+            return cypher.decrypt(text)
+    return 'This is a tool that performs encryption and decryption using RSA cypher'
 
 
 @app.teardown_appcontext
